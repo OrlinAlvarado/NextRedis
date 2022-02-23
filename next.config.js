b1a7c0b792910992d73cc7fc9bc7b790fb6bc559
Orlin Alvarado <oalvaradomorales@gmail.com>
2022-02-22 21:49:41 -0600

    Ejemplo finalizado

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.hondahonduras.com', 'carplanet.mx', 'www.placervial.com'],
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/search.js
@@ -0,0 +1,11 @@
+import SearchForm from '../lib/SearchForm'
+
+export default function Search() {
+    return (
+      <div>
+        <h1>Search a Car</h1>
+        <SearchForm />
+      </div>
+      
+    )
+  }
